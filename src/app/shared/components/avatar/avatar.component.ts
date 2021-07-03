444ef11793150fc3559b12290071c93367508eaf
Adam Szmukała <adam.szmukala@verve-capital.com>
2021-07-03 11:28:44 +0200

    performance improvements

--- src/app/shared/components/avatar/avatar.component.ts
+++ src/app/shared/components/avatar/avatar.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Person } from 'src/app/core/models/person';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AvatarComponent {
   @Input() person: Person;
